2730daf6e5d7e654cc7e2cac3433ea33a128e829
Christopher Vouga <crvouga@gmail.com>
2021-02-18 21:44:53 -0700

    created gallery schema

---
+++ studio/schemas/gallery.ts
@@ -0,0 +1,25 @@
+export default {
+  name: "gallery",
+  title: "Gallery",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      required: true,
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [
+        {
+          type: "image",
+          name: "image",
+          title: "Image",
+        },
+      ],
+    },
+  ],
+};
--- studio/schemas/schema.ts
+++ studio/schemas/schema.ts
@@ -1,10 +1,11 @@
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import createSchema from "part:@sanity/base/schema-creator";
+import gallery from "./gallery";
 import showcase from "./showcase";
 import socialMedia from "./social-media";
 import video from "./video";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([showcase, socialMedia, video]),
+  types: schemaTypes.concat([showcase, socialMedia, video, gallery]),
 });
