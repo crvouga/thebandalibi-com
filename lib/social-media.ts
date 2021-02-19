f7d4218122da1c99c98fc305b6b10cb56faf2906
Christopher Vouga <crvouga@gmail.com>
2021-02-19 14:03:33 -0700

    fixed runtime errors

--- lib/social-media.ts
+++ lib/social-media.ts
@@ -4,12 +4,17 @@ import { editDistance, minimumBy } from "./utility";
 const SOCIAL_MEDIA_IMAGE_ROOT = "public/social-media/icons";
 
 export const socialMediaNameToImagePath = (socialMediaName: string): string => {
-  const fileNames = fs.readdirSync(SOCIAL_MEDIA_IMAGE_ROOT);
+  try {
+    const fileNames = fs.readdirSync(SOCIAL_MEDIA_IMAGE_ROOT);
 
-  const closestFileName = minimumBy(
-    (fileName) => editDistance(fileName, socialMediaName),
-    fileNames
-  );
+    const closestFileName = minimumBy(
+      (fileName) => editDistance(fileName, socialMediaName),
+      fileNames
+    );
 
-  return `/social-media/icons/${closestFileName}`;
+    return `/social-media/icons/${closestFileName}`;
+  } catch (error) {
+    console.error(error.toString());
+    return "";
+  }
 };
