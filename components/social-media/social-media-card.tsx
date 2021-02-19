6b49266fbfd9162c240a80c2b0a342c70cf7652a
Christopher Vouga <crvouga@gmail.com>
2021-02-18 20:56:53 -0700

    removed icon button

--- components/social-media/social-media-card.tsx
+++ components/social-media/social-media-card.tsx
@@ -1,9 +1,7 @@
 import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
-import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
-import LaunchIcon from "@material-ui/icons/Launch";
 import Image from "next/image";
 import React from "react";
 import { ISocialMedia } from "../../lib/contracts";
@@ -37,11 +35,6 @@ export const SocialMediaCard = (props: ISocialMediaCardProps) => {
         }
         titleTypographyProps={{ variant: "h5" }}
         title={socialMedia.name}
-        action={
-          <IconButton aria-label={`${socialMedia.name} the band Alibi`}>
-            <LaunchIcon className={classes.icon} />
-          </IconButton>
-        }
       />
     </Card>
   );
