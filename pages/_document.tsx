a01e38b9a33f18d00231f0e8744677fd799595d7
Christopher Vouga <crvouga@gmail.com>
2021-02-23 12:04:13 -0700

    removed font load warning in console

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,23 +32,15 @@ export default class extends Document {
   }
 
   render() {
-    //why?: https://stackoverflow.com/questions/45983301/google-pagespeed-eliminate-render-blocking-resources-above-the-fold-caused-fro
-    const handleLoad = (
-      event: React.SyntheticEvent<HTMLLinkElement, Event>
-    ) => {
-      const link = event.currentTarget;
-      link.onload = null;
-      link.rel = "stylesheet";
-    };
-
     return (
       <Html lang="en">
         <Head>
           <link
-            rel="preload"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             as="style"
-            onLoad={handleLoad}
+            rel="stylesheet preload prefetch"
+            type="text/css"
+            crossOrigin="anonymous"
           />
         </Head>
 
