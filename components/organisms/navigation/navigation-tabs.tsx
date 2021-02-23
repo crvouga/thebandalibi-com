c0bddd17ee46eb08e2c1db4b02960ca856f71a06
Christopher Vouga <crvouga@gmail.com>
2021-02-23 13:14:41 -0700

    made nav tabs nextjs links

--- components/organisms/navigation/navigation-tabs.tsx
+++ components/organisms/navigation/navigation-tabs.tsx
@@ -1,6 +1,8 @@
-import Tab from "@material-ui/core/Tab";
+import Tab, { TabProps } from "@material-ui/core/Tab";
 import Tabs, { TabsProps } from "@material-ui/core/Tabs";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { NAVIGATION_ACTIONS } from "./navigation-constants";
 
 const a11yProps = <T,>(index: T) => {
@@ -10,40 +12,53 @@ const a11yProps = <T,>(index: T) => {
   };
 };
 
+export const NavigationTab = ({
+  href,
+  ...TabProps
+}: TabProps & { href: string }) => {
+  return (
+    <Link href={href}>
+      <Tab {...TabProps} />
+    </Link>
+  );
+};
+
 export const NavigationTabs = (props: TabsProps) => {
+  const [value, setValue] = useState(0);
   const router = useRouter();
 
-  const selected = NAVIGATION_ACTIONS.find(
-    (action) => router.pathname.split("/")[1] === action.pathname.split("/")[1]
-  );
+  const updateValue = () => {
+    const newValue = NAVIGATION_ACTIONS.findIndex(
+      (navigationAction) =>
+        router.pathname.split("/")[1] ===
+        navigationAction.pathname.split("/")[1]
+    );
+    setValue(Math.max(0, newValue));
+  };
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  useEffect(() => {
+    updateValue();
+  }, [router.pathname]);
 
   return (
     <Tabs
-      value={selected ? selected.pathname : undefined}
+      value={value}
+      onChange={handleChange}
       aria-label="navigation tabs"
       {...props}
     >
-      {NAVIGATION_ACTIONS.map(
-        ({ pathname, label, FilledIcon, OutlinedIcon }, index) => (
-          <Tab
-            key={pathname}
-            value={pathname}
-            icon={
-              undefined
-              // selected && selected.pathname === pathname ? (
-              //   <FilledIcon />
-              // ) : (
-              //   <OutlinedIcon />
-              // )
-            }
-            onClick={() => {
-              router.push(pathname);
-            }}
-            label={label}
-            {...a11yProps(index)}
-          />
-        )
-      )}
+      {NAVIGATION_ACTIONS.map(({ pathname, label }, index) => (
+        <NavigationTab
+          key={pathname}
+          href={pathname}
+          label={label}
+          {...a11yProps(index)}
+        />
+      ))}
     </Tabs>
   );
 };
