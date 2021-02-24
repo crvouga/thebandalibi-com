c77c4a5edf30d7b6db5d28ba8fbd915d257d94f2
Christopher Vouga <crvouga@gmail.com>
2021-02-24 00:32:42 -0700

    center image view on desktop

--- components/organisms/image-view.tsx
+++ components/organisms/image-view.tsx
@@ -2,11 +2,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import panzoom from "panzoom";
 import { useEffect, useRef } from "react";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   wrapper: {
     width: "100vw",
     height: "100vh",
+
     overflow: "hidden",
+    margin: "auto",
   },
 
   instance: {
@@ -14,7 +16,11 @@ const useStyles = makeStyles(() => ({
     height: "100%",
   },
 
+  imageWrapper: {},
+
   image: {
+    maxWidth: theme.breakpoints.values.sm,
+
     width: "100%",
   },
 }));
@@ -29,12 +35,17 @@ export const ImageView = ({
   const classes = useStyles();
 
   const ref = useRef<HTMLDivElement | null>(null);
+  const imageRef = useRef<HTMLImageElement | null>(null);
 
   useEffect(() => {
-    if (ref.current) {
-      const instance = panzoom(ref.current);
+    if (ref.current && imageRef.current) {
+      const instance = panzoom(ref.current, {});
 
-      console.log("HELLO");
+      const center =
+        ref.current.offsetWidth / 2 - imageRef.current.offsetWidth / 2;
+
+      instance.moveTo(center, 0);
+
       return () => {
         instance.dispose();
       };
@@ -44,7 +55,7 @@ export const ImageView = ({
   return (
     <div className={classes.wrapper}>
       <div className={classes.instance} ref={ref}>
-        <img className={classes.image} src={image} />
+        <img ref={imageRef} className={classes.image} src={image} />
       </div>
     </div>
   );
