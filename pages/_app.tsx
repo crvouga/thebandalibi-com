48796f7e1ee4942b8a656b833a6fb6ad22954b7f
Christopher Vouga <crvouga@gmail.com>
2021-02-26 15:54:24 -0700

    removed unused deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,17 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { AppLayout } from "../components/templates/layout.tsx/app-layout";
 import { ThemeProvider } from "../components/theme";
-
-const queryClient = new QueryClient();
 
 const App = (props: AppProps) => {
   const { Component, pageProps } = props;
 
   return (
-    <React.Fragment>
-      <QueryClientProvider client={queryClient}>
-        <ThemeProvider>
-          <AppLayout>
-            <Component {...pageProps} />
-          </AppLayout>
-        </ThemeProvider>
-      </QueryClientProvider>
-    </React.Fragment>
+    <ThemeProvider>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </ThemeProvider>
   );
 };
 
