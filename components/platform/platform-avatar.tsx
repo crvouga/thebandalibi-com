7d52118644837fe9a0bcd7855bb51384fc7d0e33
Christopher Vouga <crvouga@gmail.com>
2021-03-01 14:36:34 -0700

    use font awesome for social media icons

--- components/platform/platform-avatar.tsx
+++ components/platform/platform-avatar.tsx
@@ -1,14 +1,28 @@
+import { Icon } from "@material-ui/core";
 import Avatar, { AvatarProps } from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
-import Image from "next/image";
+import clsx from "clsx";
 import React from "react";
 import { IPlatform } from "../../lib/domain";
+import { minimumBy } from "../../lib/utility";
+import { editDistance } from "../../lib/utility/edit-distance";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   avatar: {
     background: "transparent",
   },
+  icon: {
+    color: theme.palette.text.primary,
+    fontSize: "36px",
+  },
 }));
+
+const BRAND_ICON_CLASSNAME: { [key: string]: string } = {
+  youtube: "fa fa-youtube-play",
+  instagram: "fa fa-instagram",
+  spotify: "fa fa-spotify",
+  apple: "fa fa-apple",
+};
 
 export const PlatformAvatar = (
   props: AvatarProps & { platform: IPlatform }
@@ -17,9 +31,19 @@ export const PlatformAvatar = (
 
   const classes = useStyles();
 
+  const iconClassNameKey = minimumBy(
+    (key) => editDistance(key, platform.name),
+    Object.keys(BRAND_ICON_CLASSNAME)
+  );
+
+  const iconClassName =
+    iconClassNameKey in BRAND_ICON_CLASSNAME
+      ? BRAND_ICON_CLASSNAME[iconClassNameKey]
+      : "not-found";
+
   return (
     <Avatar className={classes.avatar} variant="rounded" {...AvatarProps}>
-      <Image alt={platform.name} layout="fill" src={platform.icon.url} />
+      <Icon className={clsx(iconClassName, classes.icon)} />
     </Avatar>
   );
 };
