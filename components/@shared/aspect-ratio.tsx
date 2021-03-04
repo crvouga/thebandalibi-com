175ef29215ea728ee2812891b94e315011480b09
Christopher Vouga <crvouga@gmail.com>
2021-03-03 22:14:19 -0700

    cleaned up aspect ratio

--- components/@shared/aspect-ratio.tsx
+++ components/@shared/aspect-ratio.tsx
@@ -1,34 +1,6 @@
-import { makeStyles } from "@material-ui/core/styles";
 import React from "react";
 import ReactAspectRatio from "react-aspect-ratio";
 import "react-aspect-ratio/aspect-ratio.css";
-
-const useStyles = makeStyles(() => ({
-  root: {
-    boxSizing: "border-box",
-    position: "relative",
-    padding: 0,
-    margin: 0,
-  },
-
-  svg: {
-    boxSizing: "border-box",
-    width: "100%",
-    padding: 0,
-    margin: 0,
-  },
-
-  wrapper: {
-    boxSizing: "border-box",
-    padding: 0,
-    margin: 0,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-  },
-}));
 
 export const AspectRatio = ({
   ratio,
