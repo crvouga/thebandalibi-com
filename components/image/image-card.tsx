9f2d8b7a64abe45729713e62eb83229d44d9964b
Christopher Vouga <crvouga@gmail.com>
2021-03-06 15:51:30 -0700

    made image card simpler

--- components/image/image-card.tsx
+++ components/image/image-card.tsx
@@ -1,32 +1,12 @@
-import Skeleton from "@material-ui/lab/Skeleton";
 import Image from "next/image";
 import React from "react";
 import { IImage } from "../../lib/domain";
 import { AspectRatio } from "../@shared/aspect-ratio";
-import { useBoolean } from "../@shared/use-boolean";
 
 export const ImageCard = ({ image, alt }: { image: IImage; alt: string }) => {
-  const isLoaded = useBoolean(false);
   return (
-    <AspectRatio ratio={[1, 1]}>
-      <div>
-        <Image
-          onLoad={isLoaded.setTrue}
-          objectFit="cover"
-          layout="fill"
-          src={image.url}
-          alt={alt}
-        />
-
-        {!isLoaded.value && (
-          <Skeleton
-            animation="pulse"
-            variant="rect"
-            width="100%"
-            height="100%"
-          />
-        )}
-      </div>
+    <AspectRatio ratio={1}>
+      <Image objectFit="cover" layout="fill" src={image.url} alt={alt} />
     </AspectRatio>
   );
 };
