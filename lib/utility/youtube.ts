96755dbdacd5e9753ddfe456f5bb4afa7d036042
Christopher Vouga <crvouga@gmail.com>
2021-03-11 02:39:25 -0700

    changed youtube thumbnail url

--- lib/utility/youtube.ts
+++ lib/utility/youtube.ts
@@ -3,5 +3,5 @@ import getYouTubeId from "get-youtube-id";
 export const toYouTubeThumbnailUrl = (containsYouTubeId: string) => {
   const youTubeVideoId = getYouTubeId(containsYouTubeId);
 
-  return `https://img.youtube.com/vi/${youTubeVideoId}/maxresdefault.jpg`;
+  return `https://img.youtube.com/vi/${youTubeVideoId}/hqdefault.jpg`;
 };
