06e18e7cea6f65cac481ea0b706e07abb74cc524
Christopher Vouga <crvouga@gmail.com>
2021-03-14 13:48:00 -0700

    moved files around

--- lib/data-access/store.ts
+++ lib/data-access/store.ts
@@ -1,11 +1,13 @@
+import { SanityClient } from "@sanity/client";
 import {
   IImageGalleryStore,
-  IPlatformStore,
-  IReleaseStore,
-  ISettingsStore,
-} from ".";
-import { ITagStore } from "./tag";
-import { IVideoStore } from "./video";
+  ImageGalleryDataStoreSanity,
+} from "./image-gallery";
+import { IPlatformStore, PlatformDataStoreSanity } from "./platform";
+import { IReleaseStore, ReleaseDataStoreSanity } from "./release";
+import { ISettingsStore, SettingsDataStoreSanity } from "./settings";
+import { ITagStore, TagDataStoreSanity } from "./tag";
+import { IVideoStore, VideoDataStoreSanity } from "./video";
 
 export type IStore = {
   video: IVideoStore;
@@ -14,4 +16,15 @@ export type IStore = {
   release: IReleaseStore;
   platform: IPlatformStore;
   settings: ISettingsStore;
+};
+
+export const DataStoreSanity = (sanityClient: SanityClient): IStore => {
+  return {
+    video: VideoDataStoreSanity(sanityClient),
+    tag: TagDataStoreSanity(sanityClient),
+    imageGallery: ImageGalleryDataStoreSanity(sanityClient),
+    release: ReleaseDataStoreSanity(sanityClient),
+    platform: PlatformDataStoreSanity(sanityClient),
+    settings: SettingsDataStoreSanity(sanityClient),
+  };
 };
--- lib/data-store.ts
+++ lib/data-store.ts
@@ -1,24 +1,4 @@
-import { SanityClient } from "@sanity/client";
-import {
-  ImageGalleryDataStoreSanity,
-  PlatformDataStoreSanity,
-  ReleaseDataStoreSanity,
-  SettingsDataStoreSanity,
-  VideoDataStoreSanity,
-} from "./data-access";
-import { IStore } from "./data-access/store";
-import { TagDataStoreSanity } from "./data-access/tag";
+import { DataStoreSanity } from "./data-access";
 import { sanityClient } from "./sanity-client";
-
-export const DataStoreSanity = (sanityClient: SanityClient): IStore => {
-  return {
-    video: VideoDataStoreSanity(sanityClient),
-    tag: TagDataStoreSanity(sanityClient),
-    imageGallery: ImageGalleryDataStoreSanity(sanityClient),
-    release: ReleaseDataStoreSanity(sanityClient),
-    platform: PlatformDataStoreSanity(sanityClient),
-    settings: SettingsDataStoreSanity(sanityClient),
-  };
-};
 
 export const dataStore = DataStoreSanity(sanityClient);
