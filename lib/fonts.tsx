a22adfb9fb6aadc2d589a91f27adcfdc8e18c726
Christopher Vouga <crvouga@gmail.com>
2021-03-15 19:33:42 -0700

    replace mono with sans

--- lib/fonts.tsx
+++ lib/fonts.tsx
@@ -1,14 +1,32 @@
+export const fonts = [
+  {
+    href: "/fonts/bebas-neue-v2-latin-regular.woff2",
+    type: "font/woff2",
+  },
+  {
+    href: "/fonts/ibm-plex-sans-v8-latin-700.woff2",
+    type: "font/woff2",
+  },
+  {
+    href: "/fonts/ibm-plex-sans-v8-latin-500.woff2",
+    type: "font/woff2",
+  },
+];
+
 export const Fonts = () => {
   return (
     <>
-      {/* <link
-        rel="preload"
-        href="/fonts/inter-var-latin.woff2"
-        as="font"
-        type="font/woff2"
-        crossOrigin="anonymous"
-      /> */}
-      <link href="/fonts/fonts.css" rel="stylesheet" />
+      {/* Preloading fonts to combat FOUT */}
+      {fonts.map((font) => (
+        <link
+          key={font.href}
+          rel="preload"
+          as="font"
+          crossOrigin="anonymous"
+          {...font}
+        />
+      ))}
+      <link href="/fonts/fonts.css" rel="preload stylesheet" type="text/css" />
     </>
   );
 };
