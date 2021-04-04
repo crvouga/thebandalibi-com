92a42e596daa0c8bade28a3a090b50c40c80538a
Christopher Vouga <crvouga@gmail.com>
2021-04-03 20:18:32 -0700

    hide store page

--- components/navigation/navigation-constants.tsx
+++ components/navigation/navigation-constants.tsx
@@ -1,11 +1,5 @@
 import { SvgIconProps } from "@material-ui/core/SvgIcon";
-import {
-  HomeIcon,
-  ImageIcon,
-  ReleaseIcon,
-  StoreIcon,
-  VideoIcon,
-} from "../shared/icons";
+import { HomeIcon, ImageIcon, ReleaseIcon, VideoIcon } from "../shared/icons";
 
 export type INavigationAction = {
   label: string;
@@ -21,11 +15,7 @@ export const NAVIGATION_ACTIONS: INavigationAction[] = [
     pathname: "/",
     Icon: HomeIcon,
   },
-  {
-    label: "Store",
-    pathname: "/store",
-    Icon: StoreIcon,
-  },
+
   {
     label: "Music",
     pathname: "/music",
