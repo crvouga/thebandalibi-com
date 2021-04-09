be143ef1da093350c00df71a6549dff6fc3fe044
Christopher Vouga <crvouga@gmail.com>
2021-04-08 20:04:19 -0700

    added gutter for minimized video player

---
+++ components/video/video-player-modal-minimized-gutter.tsx
@@ -0,0 +1,14 @@
+import Collapse from "@material-ui/core/Collapse";
+import React from "react";
+import { Gutter } from "../navigation/gutter";
+import { useVideoState } from "./video-state";
+
+export const VideoPlayerModalMinimizedGutter = () => {
+  const videoState = useVideoState();
+
+  return (
+    <Collapse in={videoState.modalState === "minimized"}>
+      <Gutter />
+    </Collapse>
+  );
+};
