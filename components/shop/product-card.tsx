31454f1340410eaf26f1412dee992952badd705d
Christopher Vouga <crvouga@gmail.com>
2021-04-15 13:54:42 -0700

    changed product card widths

--- components/shop/product-card.tsx
+++ components/shop/product-card.tsx
@@ -12,12 +12,12 @@ export const ProductCard = ({
 }) => {
   return (
     <Box width="100%" display="flex" alignItems="center">
-      <Box width="50%">
+      <Box width={1 / 3}>
         <AspectRatio ratio={1}>
           <Image layout="fill" src={product.thumbnailUrl} alt={product.name} />
         </AspectRatio>
       </Box>
-      <Box width="50%">
+      <Box width={2 / 3}>
         <CardHeader
           title={product.name}
           subheader={plural({
