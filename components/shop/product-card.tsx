2521e2418ecc02e33051e098258cc88d098542d2
Christopher Vouga <crvouga@gmail.com>
2021-04-15 13:20:39 -0700

    made product cart smaller

--- components/shop/product-card.tsx
+++ components/shop/product-card.tsx
@@ -1,4 +1,4 @@
-import Card from "@material-ui/core/Card";
+import Box from "@material-ui/core/Box";
 import CardHeader from "@material-ui/core/CardHeader";
 import Image from "next/image";
 import React from "react";
@@ -10,35 +10,15 @@ export const ProductCard = ({
   product: { thumbnailUrl: string; name: string };
 }) => {
   return (
-    <Card variant="outlined" style={{ backgroundColor: "transparent" }}>
-      <AspectRatio ratio={1}>
-        <Image
-          layout="fill"
-          objectFit="cover"
-          src={product.thumbnailUrl}
-          alt={product.name}
-        />
-      </AspectRatio>
-      <CardHeader title={product.name} />
-    </Card>
-  );
-};
-
-export const ProductImageCard = ({
-  product,
-}: {
-  product: { thumbnailUrl: string; name: string };
-}) => {
-  return (
-    <Card>
-      <AspectRatio ratio={1}>
-        <Image
-          layout="fill"
-          objectFit="cover"
-          src={product.thumbnailUrl}
-          alt={product.name}
-        />
-      </AspectRatio>
-    </Card>
+    <Box width="100%" display="flex" alignItems="center">
+      <Box width="50%">
+        <AspectRatio ratio={1}>
+          <Image layout="fill" src={product.thumbnailUrl} alt={product.name} />
+        </AspectRatio>
+      </Box>
+      <Box width="50%">
+        <CardHeader title={product.name} />
+      </Box>
+    </Box>
   );
 };
