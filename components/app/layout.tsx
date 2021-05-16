1100ba4131e26ffe3de70394b017bb120a4335d8
Christopher Vouga <crvouga@gmail.com>
2021-05-16 12:01:15 -0700

    replaced meta with next-seo

--- components/app/layout.tsx
+++ components/app/layout.tsx
@@ -1,16 +1,19 @@
 import { ISettings } from "@core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import Head from "next/head";
+import { NextSeo } from "next-seo";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "../../lib/theme";
 import { VideoPlayerModal } from "../video/video-player/video-player-modal";
 import { Footer } from "./footer";
 import { Gutter } from "./gutter";
-import { formatTitle, Meta } from "./meta";
 import { NavigationBar, NavigationDrawer } from "./navigation";
 import { useQuerySettings } from "./settings";
+
+export const formatTitle = (...words: string[]) => {
+  return words.map((word) => word.trim()).join(" — ");
+};
 
 export const PageLayout = ({
   children,
@@ -22,20 +25,58 @@ export const PageLayout = ({
   settings: ISettings;
   hideFooter?: boolean;
 }>) => {
+  const title = formatTitle(settings.band.name, ...pageTitle);
+  const description = settings.band.description;
+
   return (
     <>
+      <NextSeo
+        title={title}
+        defaultTitle={settings.band.name}
+        description={description}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: settings.band.logo.url,
+          },
+        ]}
+        additionalMetaTags={[
+          {
+            property: "dc:creator",
+            content: settings.website.author,
+          },
+          {
+            property: "image",
+            content: settings.band.logo.url,
+          },
+        ]}
+        twitter={{
+          cardType: "summary_large_image",
+          site: settings.band.name,
+        }}
+        openGraph={{
+          url: settings.website.url,
+          type: "website",
+          title: title,
+          description: description,
+          images: [
+            {
+              url: settings.band.logo.url,
+              width: settings.band.logo.metadata.dimensions.width,
+              height: settings.band.logo.metadata.dimensions.height,
+              alt: settings.band.name,
+            },
+          ],
+          site_name: formatTitle(settings.band.name, "Official Site"),
+        }}
+      />
+
       <NavigationBar
         logoProps={{
           src: settings.band.logo.url,
           aspectRatio: settings.band.logo.metadata.dimensions.aspectRatio,
         }}
       />
-
-      <Meta settings={settings} />
-
-      <Head>
-        <title>{formatTitle(settings.band.name, ...pageTitle)}</title>
-      </Head>
 
       {children}
 
