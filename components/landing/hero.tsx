7747d01c32dad946ab351a36056efd6cc353ec2f
Christopher Vouga <crvouga@gmail.com>
2021-06-09 02:44:48 -0700

    cleaned up hero

--- components/landing/hero.tsx
+++ components/landing/hero.tsx
@@ -1,12 +1,10 @@
 import { IHero } from "@data-access";
 import { makeStyles } from "@material-ui/core";
-import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@ui";
 import Image from "next/image";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   blur: {
     filter: "blur(4px)",
   },
@@ -14,50 +12,44 @@ const useStyles = makeStyles(() => ({
     zIndex: -1,
     overflow: "hidden",
   },
+  root: {
+    maxWidth: theme.breakpoints.width("lg"),
+    margin: "auto",
+    width: "100%",
+    position: "relative",
+    padding: theme.spacing(24, 2),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  title: {
+    color: "#fff",
+  },
 }));
 
 export const Hero = ({ hero }: { hero: IHero }) => {
   const classes = useStyles();
   return (
-    <Container disableGutters>
-      <Box
-        width="100%"
-        position="relative"
-        paddingX={2}
-        paddingY={24}
-        display="flex"
-        flexDirection="column"
-        justifyContent="flex-end"
-        alignItems="center"
-      >
-        <div className={classes.imageWrapper}>
-          <Image
-            className={classes.blur}
-            priority
-            layout="fill"
-            objectFit="cover"
-            src={hero.mainImage.url}
-            alt={hero.title}
-          />
-        </div>
+    <div className={classes.root}>
+      <div className={classes.imageWrapper}>
+        <Image
+          className={classes.blur}
+          priority
+          layout="fill"
+          objectFit="cover"
+          src={hero.mainImage.url}
+          alt={hero.title}
+        />
+      </div>
 
-        <Typography style={{ color: "white" }} align="center" variant="h1">
-          {hero.title}
-        </Typography>
+      <Typography className={classes.title} align="center" variant="h1">
+        {hero.title}
+      </Typography>
 
-        <Typography
-          style={{ color: "white" }}
-          align="center"
-          variant="h3"
-          gutterBottom
-        >
-          {hero.subtitle}
-        </Typography>
-
-        <Button href={hero.callToAction.url} variant="contained" size="large">
-          {hero.callToAction.title}
-        </Button>
-      </Box>
-    </Container>
+      <Button href={hero.callToAction.url} variant="contained" size="large">
+        {hero.callToAction.title}
+      </Button>
+    </div>
   );
 };
