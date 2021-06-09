e19f82e7463f6875d2b6dccc7c349894d420dca1
Christopher Vouga <crvouga@gmail.com>
2021-06-09 00:27:27 -0700

    chnaged theme

--- components/theme/index.tsx
+++ components/theme/index.tsx
@@ -1,10 +1,1 @@
-import { Colors } from "./colors";
-import { createTheme } from "./create-theme";
-import { Fonts } from "./fonts";
-
-export const theme = createTheme({
-  primaryColor: Colors.orange,
-  backgroundColor: Colors.orange,
-  headingFont: Fonts.BebasNeue,
-  bodyFont: Fonts.Roboto,
-});
+export * from "./theme";
---
+++ components/theme/theme.ts
@@ -0,0 +1,10 @@
+import { Colors } from "./colors";
+import { createTheme } from "./create-theme";
+import { Fonts } from "./fonts";
+
+export const theme = createTheme({
+  primaryColor: Colors.pink,
+  backgroundColor: Colors.pink,
+  headingFont: Fonts.BebasNeue,
+  bodyFont: Fonts.Roboto,
+});
