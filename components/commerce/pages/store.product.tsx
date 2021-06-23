c2f6e23f5d91daa3cdbefe5aabb10fed3806eae4
Christopher Vouga <crvouga@gmail.com>
2021-06-23 04:04:33 -0700

    dryed code

--- components/commerce/pages/store.product.tsx
+++ components/commerce/pages/store.product.tsx
@@ -1,26 +1,5 @@
-import Box from "@material-ui/core/Box";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import React from "react";
-import { ISettings } from "@data-access";
-import { IProduct } from "@data-access";
-import { PageWrapper } from "../../top-level";
+import { Shop } from "./store";
 
-export type IProductProps = {
-  settings: ISettings;
-  products: IProduct[];
-};
+export type IProductProps = Parameters<typeof Shop>[0];
 
-export const Product = (props: IProductProps) => {
-  const { settings } = props;
-
-  return (
-    <PageWrapper pageTitle={["Store", "Products"]} settings={settings}>
-      <Container>
-        <Box paddingY={2}>
-          <Typography variant="h1">Products</Typography>
-        </Box>
-      </Container>
-    </PageWrapper>
-  );
-};
+export const Product = Shop;
