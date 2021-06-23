fcb7ff2e053ca63ff0ae442e35cf2bcfbbeb1747
Christopher Vouga <crvouga@gmail.com>
2021-06-22 19:41:27 -0700

    removed gateway

--- components/top-level/app/app-wrapper.tsx
+++ components/top-level/app/app-wrapper.tsx
@@ -3,7 +3,6 @@ import { MuiThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { theme } from "../theme";
-import { AppGateway } from "./app-gateway";
 import { AppLayout } from "./app-layout";
 
 export const AppWrapper = ({ children }: React.PropsWithChildren<{}>) => {
@@ -11,9 +10,8 @@ export const AppWrapper = ({ children }: React.PropsWithChildren<{}>) => {
     <QueryClientProvider client={new QueryClient()}>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <AppGateway>
-          <AppLayout>{children}</AppLayout>
-        </AppGateway>
+
+        <AppLayout>{children}</AppLayout>
       </MuiThemeProvider>
     </QueryClientProvider>
   );
