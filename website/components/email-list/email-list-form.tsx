742e1746ec109c726e283402e4764ef0c2e8c9a9
Christopher Vouga <crvouga@gmail.com>
2021-07-13 16:05:11 -0700

    added success feedback

--- website/components/email-list/email-list-form.tsx
+++ website/components/email-list/email-list-form.tsx
@@ -1,8 +1,19 @@
 import { Button } from "@components/generic";
 import { emailList } from "@data-access";
+import { useTheme } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
 import TextField from "@material-ui/core/TextField";
-import { EmailAddress, IEmailAddress, validateEmailAddress } from "@utility";
+import Typography from "@material-ui/core/Typography";
+import {
+  EmailAddress,
+  IEmailAddress,
+  useBoolean,
+  validateEmailAddress,
+} from "@utility";
 import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import { MdCheckCircle } from "react-icons/md";
 import { useMutation } from "react-query";
 
 const getFormValue = (formData: FormData, name: string) => {
@@ -16,9 +27,14 @@ const getFormValue = (formData: FormData, name: string) => {
 export const EmailListForm = () => {
   const [errors, setErrors] = useState<{ message: string }[]>([]);
 
+  const open = useBoolean(false);
+
   const mutation = useMutation({
     mutationFn: async (emailAddress: IEmailAddress) => {
       await emailList.subscribe({ emailAddress });
+    },
+    onSuccess: () => {
+      open.setTrue();
     },
   });
 
@@ -52,29 +68,72 @@ export const EmailListForm = () => {
     setErrors(errors);
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        variant="filled"
-        id="email sign up"
-        label="Email Address"
-        name="emailAddress"
-        type="email"
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginY: 1 }}
-      />
+  const theme = useTheme();
 
-      <Button
-        disabled={errors.length > 0}
-        loading={mutation.status === "loading"}
-        type="submit"
-        fullWidth
-        size="large"
-        variant="contained"
-      >
-        Subscribe
-      </Button>
-    </form>
+  return (
+    <>
+      <Dialog open={open.value} onClose={open.setFalse}>
+        <Box
+          sx={{
+            width: "100%",
+            backgroundColor: theme.palette.success.main,
+            p: 2,
+
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <MdCheckCircle
+            style={{
+              width: "100px",
+              height: "100px",
+              color: "#fff",
+            }}
+          />
+        </Box>
+        <Box sx={{ p: 2 }}>
+          <Typography
+            variant="h3"
+            align="center"
+            sx={{ color: "success.main" }}
+          >
+            Success
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="textSecondary">
+            Thank you for subscribing to our email list!
+          </Typography>
+        </Box>
+        <DialogActions>
+          <Button onClick={open.setFalse} color="inherit">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          variant="filled"
+          id="email sign up"
+          label="Email Address"
+          name="emailAddress"
+          type="email"
+          onChange={handleChange}
+          fullWidth
+          sx={{ marginY: 1 }}
+        />
+
+        <Button
+          disabled={errors.length > 0}
+          loading={mutation.status === "loading"}
+          type="submit"
+          fullWidth
+          size="large"
+          variant="contained"
+        >
+          Subscribe
+        </Button>
+      </form>
+    </>
   );
 };
