022a32027d444329f708988f1131abd339e8566e
Christopher Vouga <crvouga@gmail.com>
2021-07-20 13:52:59 -0700

    fixed timeline styles

--- website/components/content/events/event-timeline.tsx
+++ website/components/content/events/event-timeline.tsx
@@ -5,7 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Timeline from "@material-ui/lab/Timeline";
 import TimelineConnector from "@material-ui/lab/TimelineConnector";
 import TimelineContent from "@material-ui/lab/TimelineContent";
-import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineItem, { TimelineItemProps } from "@material-ui/lab/TimelineItem";
 import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
 import { toYouTubeThumbnailUrl } from "@utility";
@@ -24,15 +24,18 @@ const eventToThumbnailUrl = (event: IEvent) => {
   return null;
 };
 
-export const EventTimelineItem = ({ event }: { event: IEvent }) => {
+export const EventTimelineItem = ({
+  event,
+  ...props
+}: { event: IEvent } & TimelineItemProps) => {
   const thumbnailUrl = eventToThumbnailUrl(event);
   return (
-    <Link href={routes.singleEvent(event)}>
-      <TimelineItem>
-        <TimelineOppositeContent color="text.secondary" variant="subtitle2">
-          {new Date(event.date).toDateString()}
-        </TimelineOppositeContent>
-        <TimelineSeparator>
+    <TimelineItem {...props}>
+      <TimelineOppositeContent color="text.secondary" variant="subtitle2">
+        {new Date(event.date).toDateString()}
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        <Link href={routes.singleEvent(event)}>
           <Avatar
             sx={{ width: "64px", height: "64px" }}
             variant="rounded"
@@ -41,14 +44,16 @@ export const EventTimelineItem = ({ event }: { event: IEvent }) => {
           >
             <MdEvent style={{ width: "66.66%", height: "66.66%" }} />
           </Avatar>
+        </Link>
 
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ paddingBottom: 8 }}>
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent sx={{ paddingBottom: 8 }}>
+        <Link href={routes.singleEvent(event)}>
           <Typography variant="h4">{event.name}</Typography>
-        </TimelineContent>
-      </TimelineItem>
-    </Link>
+        </Link>
+      </TimelineContent>
+    </TimelineItem>
   );
 };
 
