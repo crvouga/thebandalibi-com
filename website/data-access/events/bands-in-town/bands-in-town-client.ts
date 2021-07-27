8dc5af16f5dce500eef15f12fed96b840517bfbc
Christopher Vouga <crvouga@gmail.com>
2021-07-27 01:22:06 -0700

    started events data access

---
+++ website/data-access/events/bands-in-town/bands-in-town-client.ts
@@ -0,0 +1,27 @@
+/* 
+
+docs: https://app.swaggerhub.com/apis-docs/Bandsintown/PublicAPI/3.0.1
+
+*/
+
+export const getBandsInTownAppId = () => {
+  const appId = process.env.BANDS_IN_TOWN_APP_ID;
+
+  if (appId) {
+    return appId;
+  }
+
+  throw new Error("process.env.BANDS_IN_TOWN_APP_ID is undefined");
+};
+
+export const getBandsInTownArtistId = () => {
+  const artistId = process.env.NEXT_PUBLIC_BANDS_IN_TOWN_ARTIST_ID;
+
+  if (artistId) {
+    return artistId;
+  }
+
+  throw new Error(
+    "process.env.NEXT_PUBLIC_BANDS_IN_TOWN_ARTIST_ID is undefined"
+  );
+};
---
+++ website/data-access/events/bands-in-town/index.ts
@@ -0,0 +1,1 @@
+export * from "./bands-in-town-client";
