917571ed80465bab81ea0b3413f0270396eb62e0
Christopher Vouga <crvouga@gmail.com>
2021-08-06 21:44:15 -0700

    fixed product card

--- website/components/commerce/cards/product-card.tsx
+++ website/components/commerce/cards/product-card.tsx
@@ -60,7 +60,6 @@ export const ProductCard = ({
           <Box
             sx={{
               backgroundColor: backgroundColorToHex(backgroundColor),
-              p: 2,
             }}
           >
             <Image
