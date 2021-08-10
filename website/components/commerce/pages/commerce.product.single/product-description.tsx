a3173d3052e2752b58e8d8c31ad74e4c52494938
Christopher Vouga <crvouga@gmail.com>
2021-08-10 01:08:50 -0700

    removed unused code

--- website/components/commerce/pages/commerce.product.single/product-description.tsx
+++ website/components/commerce/pages/commerce.product.single/product-description.tsx
@@ -1,27 +1,8 @@
 import { LABELS } from "@config";
 import { IProduct } from "@data-access";
-import Accordion from "@material-ui/core/Accordion";
 import Paper from "@material-ui/core/Paper";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import React from "react";
-
-export const _ProductDescription = ({ product }: { product: IProduct }) => {
-  return (
-    <>
-      <Accordion sx={{ overflow: "hidden" }} variant="outlined">
-        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography variant="h4">{LABELS.productDescription}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <div dangerouslySetInnerHTML={{ __html: product.descriptionHTML }} />
-        </AccordionDetails>
-      </Accordion>
-    </>
-  );
-};
 
 export const ProductDescription = ({ product }: { product: IProduct }) => {
   return (
