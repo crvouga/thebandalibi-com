97c7bcd4d50e97e2e045331d8727a7ca4e25e836
Christopher Vouga <crvouga@gmail.com>
2021-08-09 22:30:54 -0700

    changed product description

--- website/components/commerce/pages/commerce.product.single/product-description.tsx
+++ website/components/commerce/pages/commerce.product.single/product-description.tsx
@@ -1,13 +1,14 @@
 import { LABELS } from "@config";
 import { IProduct } from "@data-access";
 import Accordion from "@material-ui/core/Accordion";
+import Paper from "@material-ui/core/Paper";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import React from "react";
 
-export const ProductDescription = ({ product }: { product: IProduct }) => {
+export const _ProductDescription = ({ product }: { product: IProduct }) => {
   return (
     <>
       <Accordion sx={{ overflow: "hidden" }} variant="outlined">
@@ -18,6 +19,20 @@ export const ProductDescription = ({ product }: { product: IProduct }) => {
           <div dangerouslySetInnerHTML={{ __html: product.descriptionHTML }} />
         </AccordionDetails>
       </Accordion>
+    </>
+  );
+};
+
+export const ProductDescription = ({ product }: { product: IProduct }) => {
+  return (
+    <>
+      <Paper variant="outlined" sx={{ p: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          {LABELS.productDescription}
+        </Typography>
+
+        <div dangerouslySetInnerHTML={{ __html: product.descriptionHTML }} />
+      </Paper>
     </>
   );
 };
