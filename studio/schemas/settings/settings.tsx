4d809a49fdd4e6d1cf08691f6ae7ba700b64d27c
Christopher Vouga <crvouga@gmail.com>
2021-08-12 03:42:42 -0700

    added customizable rewrites

--- studio/schemas/settings/settings.tsx
+++ studio/schemas/settings/settings.tsx
@@ -12,6 +12,56 @@ export default {
 
   fields: [
     {
+      name: "rewrites",
+      type: "array",
+      description: "Next.js rewrites configuration",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: "source",
+              type: "string",
+            },
+            {
+              name: "destination",
+              type: "string",
+            },
+          ],
+          preview: {
+            select: {
+              source: "source",
+              destination: "destination",
+            },
+            prepare({
+              source,
+              destination,
+            }: {
+              source?: string;
+              destination?: string;
+            }) {
+              if (!source) {
+                return {
+                  title: "Please provide a source",
+                };
+              }
+
+              if (!destination) {
+                return {
+                  title: "Please provide a destination",
+                };
+              }
+
+              return {
+                title: `"${source}" -> "${destination}"`,
+              };
+            },
+          },
+        },
+      ],
+    },
+
+    {
       name: "band",
       type: "bandSettings",
       title: "Band Settings",
--- website/next.config.js
+++ website/next.config.js
@@ -1,10 +1,47 @@
+const SanityClient = require("@sanity/client");
+
+const santityClient = SanityClient({
+  projectId: "mswm483g",
+  dataset: "production",
+  useCdn: false,
+  apiVersion: "2021-03-25",
+});
+
+const getRewrites = async () => {
+  const query = `
+      *[_type == 'settings'] {
+        rewrites
+      }[0]
+    `;
+
+  const results = await santityClient.fetch(query);
+
+  const rewrites = results.rewrites
+    .filter(rewrite => {
+      return typeof rewrite.source === 'string' && typeof rewrite.destination === 'string';
+    })
+    .map(rewrite => {
+      return {
+        source: rewrite.source,
+        destination: rewrite.destination,
+      };
+    });
+
+  return rewrites;
+};
+
 module.exports = {
   images: {
     domains: [
       "cdn.shopify.com",
       "cdn.sanity.io",
       "img.youtube.com",
-      "fontmeme.com",
     ],
   },
+
+  async rewrites() {
+    const rewrites = await getRewrites();
+
+    return rewrites;
+  }
 };
